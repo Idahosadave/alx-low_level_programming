c10b1f93c5150775a6b99b6a41aefcf0aa9840bc
Idahosadave <idahosadave@yahoo.com>
2022-12-09 20:49:56 -0800

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - Entry
  *
  * Return: Always 0
  */
-
 int main(void)
 {
+	int c = 0;
 	int f_d;
 	int 1_d;
+
 	int c2;
 	int f_d2;
 	int 1_d2;
-	int c = 0;
 
 	while (c <= 98)
 	{
 		f_d = (c / 10 + '0');
 		1_d = (c % 10 + '0');
-
 		c2 = 0;
 
 		while (c2 <= 99)
