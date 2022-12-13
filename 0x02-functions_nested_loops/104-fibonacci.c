cb04b25d9f846876737221585ee52d6f9b8d8c78
Idahosadave <idahosadave@yahoo.com>
2022-12-13 08:15:41 -0800

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -36,8 +36,8 @@ int main(void)
 	while (i <= 98)
 	{
 		if (f1o > 0)
-			printf("%lu", f10);
-		initial0s = numlength(mx) - 1 - numlength(f1);
+			printf("%lu", f1o);
+		initial0s = fibonacci(mx) - 1 - fibonacci(f1);
 		while (f1o > 0 && initial0s > 0)
 		{
 			printf("%i", 0);
