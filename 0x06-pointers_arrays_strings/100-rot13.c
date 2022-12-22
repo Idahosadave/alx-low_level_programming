3e758da0cbf09633764d1307536b4052eed69093
Idahosadave <idahosadave@yahoo.com>
2022-12-21 20:38:19 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,39 +1,39 @@
 #include "main.h"
 
 /**
- * rot13 - Encodes a string using rot 13
+ * rot13 - Encodes a string
  * @str: the string to be ecncoded
  * Return: A pointer to the encoded string
  */
-char *rot13(char *str)
+char *rot13(char *);
 {
-	int indx1, indx2;
+	int index1, index2;
 
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-		'G', 'H', 'I', 'J', 'K', 'L',
-		'M', 'N', 'O', 'P', 'Q', 'R',
-		'S', 'T', 'U', 'V', 'W', 'X',
-		'Y', 'Z', 'a', 'b', 'c', 'd',
-		'e', 'f', 'g', 'h', 'i', 'j',
-		'k', 'l', 'm', 'n', 'o', 'p',
-		'q', 'r', 's', 't', 'u', 'v',
-		'w', 'x', 'y', 'z'};
-	char ro13key[52] = {'N', 'O', 'P', 'Q', 'R','S',
-		'T', 'U', 'V', 'W', 'X', 'Y',
-		'Z', 'A', 'B', 'C', 'D', 'E',
-		'F', 'G', 'H', 'I', 'J', 'K',
-		'L', 'M', 'n', 'o', 'p', 'q',
-		'r', 's', 't', 'u', 'v', 'w',
-		'x', 'y', 'z', 'a', 'b', 'c',
-		'd', 'e', 'f', 'g', 'h', 'i',
-		'j', 'k', 'l', 'm'};
-	while (str[++indx1])
+	char alphabet[52] = {						'A', 'B', 'C', 'D', 'E', 'F',
+									'G', 'H', 'I', 'J', 'K', 'L',
+									'M', 'N', 'O', 'P', 'Q', 'R',
+									'S', 'T', 'U', 'V', 'W', 'X',
+									'Y', 'Z', 'a', 'b', 'c', 'd',
+									'e', 'f', 'g', 'h', 'i', 'j',
+									'k', 'l', 'm', 'n', 'o', 'p',
+									'q', 'r', 's', 't', 'u', 'v',
+									'w', 'x', 'y', 'z' };
+	char ro13key[52] = {						'N', 'O', 'P', 'Q', 'R', 'S',
+									'T', 'U', 'V', 'W', 'X', 'Y',
+									'Z', 'A', 'B', 'C', 'D', 'E',
+									'F', 'G', 'H', 'I', 'J', 'K',
+									'L', 'M', 'n', 'o', 'p', 'q',
+									'r', 's', 't', 'u', 'v', 'w',
+									'x', 'y', 'z', 'a', 'b', 'c',
+									'd', 'e', 'f', 'g', 'h', 'i',
+									'j', 'k', 'l', 'm' };
+	while (str[++index1])
 	{
-		for (indx2 = 0; indx2 < 52; indx2++)
+		for (indxe2 = 0; index2 < 52; index2++)
 		{
-			if (str[indx1] == alphabet[indx2])
+			if (str[index1] == alphabet[index2])
 			{
-				str[indx1] = rot13key[indx2];
+				str[index1] = rot13key[index2];
 				break;
 			}
 		}
