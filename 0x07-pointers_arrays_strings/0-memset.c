accff18b11569cfb1cd8dea4ce2e00849ecbad9f
Idahosadave <idahosadave@yahoo.com>
2023-01-03 14:22:48 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,12 +10,12 @@
  */
 char *_memset(void *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	int i = 0;
 
-	while (i < n)
+	for (; n > 0; i++)
 	{
-		*(s + i) = b;
-		i++;
+		s[i] = b;
+		n--;
 	}
 	return (s);
 }
