fb0b0771412e9755cb646cd93ec1a165045c5227
Idahosadave <idahosadave@yahoo.com>
2023-01-06 10:43:16 -0800

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,13 +5,14 @@
  * main - prints the name of the program
  * @argc: count arguments
  * @argv: Arguments
+ *
  * Return: Always 0 (success)
  */
 
 int main(int argc, char *argv[])
 {
-(void) argv; /*ignore argv*/
-printf("%i\n", argc - 1);
+(void) argv;/*ignore argv*/
+printf("%d\n", argc - 1);
 
 return (0);
 }
